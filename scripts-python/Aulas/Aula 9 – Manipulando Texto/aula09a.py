7a8903de0607e570b4feb9b3b94608452a2728be
Gabriel Costa <gabriel.gacs12@gmail.com>
2024-12-16 15:48:18 -0300

    Aula 09 e Desafios

---
+++ scripts-python/Aulas/Aula 9 – Manipulando Texto/aula09a.py
@@ -0,0 +1,15 @@
+frase = "Gabriel estudando Python!"
+print(frase[::2])
+print(frase.count('o'))
+
+print('''\nOuça minhas palavras e seja testemunha do meu juramento.
+A noite se aproxima, e agora minha vigia começa.
+Ela não terminará até minha morte.
+Não tomarei esposa, não terei terras, nem gerarei filhos.
+Não usarei coroas e não conquistarei glória.
+Viverei e morrerei no meu posto.
+Eu sou a espada na escuridão.
+Eu sou o vigia nas muralhas.
+Eu sou o escudo que guarda os reinos dos homens.
+Eu prometo minha vida e honra à Patrulha da Noite,
+Por esta noite e por todas as noites que virão.''')
---
+++ scripts-python/Desafios/Aula 9 – Manipulando Texto/D027.py
@@ -0,0 +1,4 @@
+# Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+# EX: Ana Maria de Souza
+# Primeiro: Ana
+# Último: Souza
