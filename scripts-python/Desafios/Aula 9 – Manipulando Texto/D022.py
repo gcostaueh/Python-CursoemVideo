020e16092501426a47c9f5cf6890c1b4d9c6a095
Gabriel Costa <gabriel.gacs12@gmail.com>
2024-12-18 11:39:50 -0300

    D022
    Desafio 022 realizado!

--- scripts-python/Desafios/Aula 9 – Manipulando Texto/D022.py
+++ scripts-python/Desafios/Aula 9 – Manipulando Texto/D022.py
@@ -4,3 +4,10 @@
 # Quantas letras ao todo (sem considerar espaços).
 # Quantas letras tem o primeiro nome.
 
+nome = input('Digite o seu nome completo: ')
+primeiro_nome = nome.split()[0]
+
+print(f'Seu nome com todas as letras MAIÚSCULAS: {nome.upper()}')
+print(f'Seu nome com todas as letras minúsculas: {nome.lower()}')
+print(f'Seu nome possui {len(nome.replace(' ', ''))} letras')
+print(f'Seu primeiro nome é {primeiro_nome} e a quantidade de letras é {len(primeiro_nome)}')
